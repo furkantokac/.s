dd7f63d17e5068dcc5245b5cdff5de80e1ae388a
furkantokac <furkantokac34@gmail.com>
2020-05-19 14:05:46 +0300

    uppp.sh aptitude support.

--- uppp.sh
+++ uppp.sh
@@ -1,14 +1,28 @@
-echo -e "-----------------> sudo pkcon refresh -y"
-sudo pkcon refresh -y
+### PKCON
+# echo -e "-----------------> sudo pkcon refresh -y"
+# sudo pkcon refresh -y
+# 
+# echo -e "-----------------> pkcon get-updates"
+# pkcon get-updates 2>&1 | tee .uppp.sh.log
+# 
+# echo -e "-----------------> sudo pkcon update -y"
+# sudo pkcon update -y
+# 
+# echo -e "-----------------> sudo apt autoremove -y"
+# sudo apt autoremove -y
+# 
+# echo -e "-----------------> sudo apt autoclean -y"
+# sudo apt autoclean -y
 
-echo -e "-----------------> pkcon get-updates"
-pkcon get-updates 2>&1 | tee .uppp.sh.log
+### APTITUDE
+echo -e "-----------------> sudo aptitude update -y"
+sudo aptitude update -y
 
-echo -e "-----------------> sudo pkcon update -y"
-sudo pkcon update -y
+echo -e "-----------------> apt list --upgradable"
+apt list --upgradable 2>&1 | tee .uppp.sh.log
 
-echo -e "-----------------> sudo apt autoremove -y"
-sudo apt autoremove -y
+echo -e "-----------------> sudo aptitude upgrade -y"
+sudo aptitude upgrade -y
 
-echo -e "-----------------> sudo apt autoclean -y"
-sudo apt autoclean -y
+echo -e "-----------------> sudo aptitude autoclean -y"
+sudo aptitude autoclean -y
